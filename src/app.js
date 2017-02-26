3d6d3d688f8a1f03d0e7843a6583af16ee4eefa8
赵吉彤 <zjtong3576@sina.com>
2017-02-26 10:05:47 +0800

    up

--- src/app.js
+++ src/app.js
@@ -1,25 +1,35 @@
 var vm = new Vue({
     el: '#app',
     data: {
-        config: [],
-        ss: "./static/images/p1.jpg"
+        config: []
     },
     created: function () {
-        console.log("sssssss")
         this.$http.get('./static/config/config.json').then((response) => {
-            console.log(vm.config)
             vm.config = response.body
             console.log(vm.config)
-            // this.$set('config',response.body)
         }, (response) => {
-            alert('魔借出了点小错误')
+            alert('魔借出了点小问题')
         });
     },
     methods: {
-        toProPage: function (id) {
-            if (!id) return
-            alert(id)
+        toProPage: function (id, name) {
+            if (!id && !name) return
 
+            var u = navigator.userAgent;
+            var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+            var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+            if (isAndroid && !isiOS) {
+                alert('Android:' + (id ? ('物品 id：' + id) : '') + (name ? (',物品名称：' + name) : name))
+                Android.search(name);
+            } else if (isiOS && !isAndroid) {
+                alert('ios:' + (id ? ('物品 id：' + id) : '') + (name ? (',物品名称：' + name) : name))
+                window.NativeBridge('fetchMessage', {
+                    'type': 1,
+                    'id': id
+                });
+            } else {
+                console.log('Some Error:Not Android or Ios')
+            }
         }
     }
 })
